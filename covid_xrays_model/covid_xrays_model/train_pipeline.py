6a489a5fd1e371a8b503526bf11e52e291318af6
doaa-altarawy <doaa.altarawy@gmail.com>
2020-06-08 11:24:11 -0400

    Use torch parallel in training

--- covid_xrays_model/covid_xrays_model/train_pipeline.py
+++ covid_xrays_model/covid_xrays_model/train_pipeline.py
@@ -14,7 +14,7 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def run_training_sample(sample_size=300, image_size=400):
+def run_training_sample(sample_size=600, image_size=480):
     labels = pd.read_csv(config.PROCESSED_DATA_DIR / 'labels_full.csv')
 
     classes = ['pneumonia', 'normal', 'COVID-19']
@@ -35,8 +35,9 @@ def run_training_sample(sample_size=300, image_size=400):
     data = ImageDataBunch.from_csv(config.PROCESSED_DATA_DIR, ds_tfms=tfms, size=image_size)
 
     learn = cnn_learner(data, models.resnet18, metrics=accuracy)
+    learn.model = torch.nn.DataParallel(learn.model)
 
-    learn.fit(1)
+    learn.fit(10)
 
     save_file_name = f'{config.PIPELINE_SAVE_FILE}{_version}.pkl'
     save_path = config.TRAINED_MODEL_DIR / save_file_name
