1bd7e6b2e54cd3f787c0ab905591865584f410bb
doaa-altarawy <doaa.altarawy@gmail.com>
2020-06-08 15:38:40 -0400

    plot learning rate and conf matrix

--- covid_xrays_model/covid_xrays_model/train_pipeline.py
+++ covid_xrays_model/covid_xrays_model/train_pipeline.py
@@ -14,7 +14,7 @@ import pandas as pd
 logger = logging.getLogger(__name__)
 
 
-def run_training_sample(sample_size=600, image_size=480):
+def run_training_sample(sample_size=600, image_size=400):
     labels = pd.read_csv(config.PROCESSED_DATA_DIR / 'labels_full.csv')
 
     classes = ['pneumonia', 'normal', 'COVID-19']
@@ -32,18 +32,31 @@ def run_training_sample(sample_size=600, image_size=480):
     #                       max_lighting=0.1, max_zoom=1.05, max_warp=0.
                          )
 
-    data = ImageDataBunch.from_csv(config.PROCESSED_DATA_DIR, ds_tfms=tfms, size=image_size)
+    data = ImageDataBunch.from_csv(config.PROCESSED_DATA_DIR,
+                                   ds_tfms=tfms,
+                                   size=image_size)
+
+    data.normalize()
 
     learn = cnn_learner(data, models.resnet18, metrics=accuracy)
     learn.model = torch.nn.DataParallel(learn.model)
 
-    learn.fit(10)
+    # learn.fit(10)
+    learn.fit_one_cycle(10)
+
+    learn.unfreeze() # must be done before calling lr_find
+    learn.lr_find()
+    learn.recorder.plot(return_fig=True).savefig('learning_rate.png', dpi=300)
+
+    # learn.fit_one_cycle(10, max_lr=slice(3e-3, 3e-2))
 
     save_file_name = f'{config.PIPELINE_SAVE_FILE}{_version}.pkl'
     save_path = config.TRAINED_MODEL_DIR / save_file_name
 
     learn.export(save_path)
 
+    interp = ClassificationInterpretation.from_learner(learn)
+    interp.plot_confusion_matrix(return_fig=True).savefig('confusion_matrix.png', dpi=200)
 
 
 if __name__ == '__main__':
